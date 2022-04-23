f587b9a972462048cf6fb75ec6596f17099dfcb3
robcabobb <robcabobb@gmail.com>
2022-04-22 22:58:20 -0400

    started code to have page save products if there isn't a connection established

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -12,6 +12,8 @@ import {
 } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
+import { idbPromise } from "../utils/helpers";
+
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -24,15 +26,32 @@ function Detail() {
   const { products, cart } = state;
 
   useEffect(() => {
+    // already in global store
     if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    } else if (data) {
+      setCurrentProduct(products.find(product => product._id === id));
+    } 
+    // retrieved from server
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products,
+        products: data.products
+      });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
       });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
+  
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id);
