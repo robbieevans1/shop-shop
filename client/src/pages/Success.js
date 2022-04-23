0bc7bac99f4cf5d2f01fde2685f509595ce4f4ff
robcabobb <robcabobb@gmail.com>
2022-04-23 00:00:56 -0400

    started adding code to integrate stripe checkout. installed on client and server side

---
+++ client/src/pages/Success.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import Jumbotron from '../components/Jumbotron';
+import { ADD_ORDER } from '../utils/mutations';
+import { idbPromise } from '../utils/helpers';
+
+function Success() {
+  const [addOrder] = useMutation(ADD_ORDER);
+
+useEffect(() => {
+  async function saveOrder() {
+
+  }
+
+  saveOrder();
+}, [addOrder]);
+
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Success!</h1>
+        <h2>
+          Thank you for your purchase!
+        </h2>
+        <h2>
+          You will now be redirected to the homepage
+        </h2>
+      </Jumbotron>
+    </div>
+  );
+};
