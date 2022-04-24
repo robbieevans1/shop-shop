179531ec76e0464f784534bf149a4b59b8b48c27
robcabobb <robcabobb@gmail.com>
2022-04-24 07:26:13 -0400

    run build command

---
+++ client/build/precache-manifest.6275fce6ff0a1a8add634e2578cab2c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968a04c2386562cd59cdf905cee76487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21a7c806087983b3f5f0",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "938ecc4a6325770f787a",
+    "url": "/static/js/2.55318643.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.55318643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a7c806087983b3f5f0",
+    "url": "/static/js/main.147743df.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
